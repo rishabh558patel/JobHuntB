34fdf036ebad9698f9669b081018adef456157f9
Rishabh Patel <rishabh558patel@gmail.com>
2025-08-07 12:59:36 +0530

    update CORS to allow frontend

--- index.js
+++ index.js
@@ -14,12 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: 'https://jobhuntf.vercel.app',
+  credentials: true, // only if you're using cookies
+}));
 
 const PORT = process.env.PORT || 3000;
 
